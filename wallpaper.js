de046723f5f2db9791209bf0230add4ada6255ef
ksk-aiko <step.up.to.brighter03@gmail.com>
2025-12-03 18:04:46 +0900

    Refactor wallpaper list creation to eliminate intermediate variable declarations for improved clarity

--- wallpaper.js
+++ wallpaper.js
@@ -77,30 +77,30 @@ class WallPaperHelper {
   }
 }
 
-// Use const for variables that are not reassigned
-const wallPaper1 = new WallPaper(
-  "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away. - Antoine de Saint",
-  "1B4F72",
-  "https://cdn.pixabay.com/photo/2020/06/12/03/06/magnifying-glass-5288877__340.jpg",
-  "top",
-  "right"
-);
-
-const wallPaper2 = new WallPaper(
-  "The scientist discovers a new type of material or energy and the engineer discovers a new use for it. - Gordon Lindsay Glegg",
-  "007bff",
-  "https://cdn.pixabay.com/photo/2018/02/23/04/38/laptop-3174729_1280.jpg",
-  "center",
-  "left"
-);
-
-const wallPaper3 = new WallPaper(
-  "Scientists study the world as it is, engineers create the world that never has been. - Theodore von Karman",
-  "ecf0f1",
-  "https://cdn.pixabay.com/photo/2017/05/10/19/29/robot-2301646_1280.jpg",
-  "center",
-  "center"
-);
-const wallPaperList = [wallPaper1, wallPaper2, wallPaper3];
+// Create wallpaper list directly without intermediate variables
+// This reduces unnecessary variable declarations
+const wallPaperList = [
+  new WallPaper(
+    "Perfection is achieved, not when there is nothing more to add, but when there is nothing left to take away. - Antoine de Saint",
+    "1B4F72",
+    "https://cdn.pixabay.com/photo/2020/06/12/03/06/magnifying-glass-5288877__340.jpg",
+    "top",
+    "right"
+  ),
+  new WallPaper(
+    "The scientist discovers a new type of material or energy and the engineer discovers a new use for it. - Gordon Lindsay Glegg",
+    "007bff",
+    "https://cdn.pixabay.com/photo/2018/02/23/04/38/laptop-3174729_1280.jpg",
+    "center",
+    "left"
+  ),
+  new WallPaper(
+    "Scientists study the world as it is, engineers create the world that never has been. - Theodore von Karman",
+    "ecf0f1",
+    "https://cdn.pixabay.com/photo/2017/05/10/19/29/robot-2301646_1280.jpg",
+    "center",
+    "center"
+  ),
+];
 
 WallPaperHelper.showAll(wallPaperList);
